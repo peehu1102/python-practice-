88a8be4312162c104c893165b8c747b5f47115a0
peehu1102 <Pihupiuu@gmail.com>
2025-07-09 18:11:01 +0530

    Create faces.py

---
+++ Assignments/lesson 1/faces.py
@@ -0,0 +1,11 @@
+def main():
+    text=input("how are you doing today buddy? ")
+    text=convert(text)
+    print(text)
+
+def convert(text):
+    text=text.replace(":)","🙂")
+    text=text.replace(":(","🙁")
+
+    return text
+main()
